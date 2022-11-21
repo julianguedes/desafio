594e8b63404a07fc30029448bcc905a1578e0e46
Julian Guedes <julian.guedes.2016@gmail.com>
2022-11-21 10:52:10 -0300

    Stores, Updates Requests adict

---
+++ app/Http/Requests/UpdateAddressRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'street' => ['required', 'string'],
+            'number' => ['required', 'integer', 'gt:0'],
+            'city' => ['required', 'string'],
+            'state' => ['required', 'string']
+        ];
+    }
+}
