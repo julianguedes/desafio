2f9e3003d54fc683a96376cd7d63624b8ac4de2c
Julian Guedes <julian.guedes.2016@gmail.com>
2022-12-21 11:49:54 -0300

    atualizando rotas

--- routes/api.php
+++ routes/api.php
@@ -6,41 +6,12 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\AddressController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-
-
-// Route::apiResource('users', UserController::class);
-// Route::apiResource('tasks', TasksController::class);
-
-// Route::apiResource('addresses', AddressController::class);
-
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{user}', [UserController::class, 'show']);
-Route::get('/users-emails', [UserController::class, 'getUserEmails']);
+Route::apiResource('users', UserController::class);
 Route::get('/users/{user}/infos', [UserController::class, 'showUserInfos']);
-Route::put('/users/{user}', [UserController::class, 'update']);
-Route::delete('/users/{user}', [UserController::class, 'destroy']);
 
-Route::get('/addresses', [AddressController::class, 'index']);
-Route::post('/addresses', [AddressController::class, 'store']);
-Route::get('/addresses/{address}', [AddressController::class, 'show']);
-Route::put('/addresses/{address}', [AddressController::class, 'update']);
-Route::delete('/addresses/{address}', [AddressController::class, 'destroy']);
+Route::apiResource('addresses', AddressController::class);
 
-Route::get('/tasks', [TaskController::class, 'index']);
-Route::post('/tasks', [TaskController::class, 'store']);
-Route::get('/tasks/{task}', [TaskController::class, 'show']);
-Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::apiResource('tasks', TaskController::class);
 Route::put('/tasks/{task}/task-completed', [TaskController::class, 'taskCompleted']);
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+
